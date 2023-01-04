f8313864a0f160dc74b8594cafcaeefaa5d18d85
Biswaraj <biswaraj2013@gmail.com>
2023-01-04 17:06:31 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  // screens: {
+  //   xsm: '375px',
+  //   sm: '640px',
+  //   md: '768px',
+  //   lg: '1024px',
+  //   xl: '1280px',
+  //   '2xl': '1536px',
+  // },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
